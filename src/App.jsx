72b84fbbb5ab96bb6bb364f9ddd523660662e3c4
Utkarsh Bhimte <utkarshbhimte95@gmail.com>
2017-10-12 03:24:52 +0530

    the gyroscope is working

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      icons: []
+      icons: [],
+      sensor: {}
     };
 
     this.width = window.innerWidth;
@@ -48,6 +49,32 @@ class App extends Component {
   componentDidMount = () => {
     this._importIcons();
     this._getheadingPosition();
+
+    window.addEventListener("deviceorientation", this._handleOrientation, true);
+  };
+
+  _handleOrientation = event => {
+    // const changeValue = (currValue, newValue) => {
+    //   if (Math.abs(currValue - newValue) > 0.1) return newValue;
+    //   else currValue || 0;
+    // };
+
+    // var absolute = changeValue("absolute");
+    // var alpha = changeValue("alpha");
+    // var beta = changeValue("beta");
+    // var gamma = changeValue("gamma");
+
+    // Do stuff with the new orientation data
+    const { absolute, alpha, beta, gamma } = event;
+
+    // const sensor = {
+    //   absolute: changeValue(this.state.absolute, absolute),
+    //   alpha: changeValue(this.state.alpha, alpha),
+    //   beta: changeValue(this.state.beta, beta),
+    //   gamma: changeValue(this.state.gamma, gamma)
+    // };
+
+    this.setState({ sensor: { absolute, alpha, beta, gamma } });
   };
 
   // helper
@@ -57,22 +84,27 @@ class App extends Component {
     const half_height = this.height / 2;
     const half_width = this.width / 2;
 
-    const x_state = Math.round(quad / 2) - 1;
-    const y_state = quad % 2 === 0 ? 1 : 0;
+    const x_state = Math.round(quad / 2) !== 1 ? true : false;
+    const y_state = quad % 2 === 0 ? true : false;
 
-    const xAxis = x_state * half_width + Math.random() * half_width;
-    const yAxis = y_state * half_height + Math.random() * half_height;
+    const xAxis = Math.random() * half_width;
+    const yAxis = Math.random() * half_height + 50;
 
-    // console.log("quad", quad, x_state, y_state);
+    console.log("quad", quad, x_state, y_state);
 
-    return { xAxis, yAxis };
+    return {
+      xAxis,
+      yAxis,
+      vert: y_state ? "left" : "right",
+      hor: x_state ? "top" : "bottom"
+    };
   };
 
   _getheadingPosition = () => {
     const heading = document.querySelector(".heading");
     const heading_height = heading.clientHeight;
     const heading_offset = heading.offsetTop;
-    console.log({heading_height, heading_offset });
+    console.log({ heading_height, heading_offset });
   };
 
   //TODO: have a fcntion to make sure it is not coinciding with the heading. . .
@@ -80,29 +112,33 @@ class App extends Component {
     return { xAxis, yAxis };
   };
 
-  renderImage = (icon, i) => {
-    const { xAxis, yAxis } = this._getRandomCoords(i);
-
-    const style = { top: yAxis, left: xAxis };
-    const zIndex = sample(zIndexs)
-
-    // console.log({
-    //   top: `${yAxis} out of ${this.height / 2}/${this.height}`,
-    //   left: `${xAxis} out of ${this.width / 2}/${this.width}`
-    // });
-
+  renderImage = ({ xAxis, yAxis, target, i, hor, vert }) => {
+    console.log({ xAxis, yAxis, target, i, hor, vert });
     return (
-      <img key={i} style={style} className="space-icon" src={icon} alt="" />
+      <img
+        key={i}
+        style={{
+          [hor]: xAxis,
+          [vert]: yAxis,
+        }}
+        className="space-icon"
+        src={target}
+        alt=""
+      />
     );
   };
 
   _importIcons = async () => {
-    icons = sampleSize(icons, random(7, 12));
+    icons = sampleSize(icons, Math.round(Math.random() * 5) + 5);
     icons = await Promise.all([
-      ...icons.map(async icon => await require(`./images/space-icons/${icon}`))
+      ...icons.map(async (icon, i) => ({
+        ...this._getRandomCoords(i),
+        i,
+        target: await require(`./images/space-icons/${icon}`)
+      }))
     ]);
 
-    // console.log({ icons });
+    console.log({ icons });
     this.setState({ icons });
   };
 
@@ -113,7 +149,12 @@ class App extends Component {
           <div className="container">
             {/* <img src={logo} className="app-logo" alt="logo" /> */}
             <div className="card">
-              <h1 className="heading">Sample Text here</h1>
+              <h1 className="heading">Loading. . .</h1>
+              <span>
+                abs: {this.state.sensor.absolute}, <br /> alpha:{" "}
+                {this.state.sensor.alpha},<br /> beta: {this.state.sensor.beta},<br />{" "}
+                gamma: {this.state.sensor.gamma}{" "}
+              </span>
             </div>
 
             {this.state.icons.length > 0 &&
